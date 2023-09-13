bae0dfd24d8e940504863b8b19dfeb8a5fcb175e
JustAWaifuHunter <hcsbrian4056@gmail.com>
2023-09-12 23:19:53 -0300

    feat(Constants): :sparkles: update op codes

--- src/constants.ts
+++ src/constants.ts
@@ -302,8 +302,42 @@ export enum StandardEntryClassCode {
   PPD = "PPD",
 }
 
+export enum UpdateOPCodes {
+  /**
+   * Depending on the target location reference, completes one of these functions:
+   *
+   * - The target location is an array index. Inserts a new value into the array at the specified index.
+   * - The target location is an object parameter that does not already exist. Adds a new parameter to the object.
+   * - The target location is an object parameter that does exist. Replaces that parameter's value.
+   *
+   * The `value` parameter defines the value to add. For more information, see [4.1. add](https://datatracker.ietf.org/doc/html/rfc6902#section-4.1).
+   */
+  ADD = "add",
+  /**
+   * Removes the value at the target location. For the operation to succeed, the target location must exist. For more information, see [4.2. remove](https://datatracker.ietf.org/doc/html/rfc6902#section-4.2).
+   */
+  REMOVE = "remove",
+  /**
+   * Replaces the value at the target location with a new value. The operation object must contain a `value` parameter that defines the replacement value. For the operation to succeed, the target location must exist. For more information, see [4.3. replace](https://datatracker.ietf.org/doc/html/rfc6902#section-4.3).
+   */
+  REPLACE = "replace",
+  /**
+   * Removes the value at a specified location and adds it to the target location. The operation object must contain a `from` parameter, which is a string that contains a JSON pointer value that references the location in the target document `from` which to move the value. For the operation to succeed, the from location must exist. For more information, [see 4.4. move](https://datatracker.ietf.org/doc/html/rfc6902#section-4.4).
+   */
+  MOVE = "move",
+  /**
+   * Copies the value at a specified location to the target location. The operation object must contain a from parameter, which is a string that contains a JSON pointer value that references the location in the target document from which to copy the value. For the operation to succeed, the from location must exist. For more information, see [4.5. copy](https://datatracker.ietf.org/doc/html/rfc6902#section-4.5).
+   */
+  COPY = "copy",
+  /**
+   * Tests that a value at the target location is equal to a specified value. The operation object must contain a `value` parameter that defines the value to compare to the target location's value. For the operation to succeed, the target location must be equal to the `value` value. For test, equal indicates that the value at the target location and the value that `value` defines are of the same JSON type. The data type of the value determines how equality is defined:
+   */
+  TEST = "test",
+}
+
 export namespace Endpoints {
   export const ORDER_CHECKOUT = "checkout/orders";
+  export const ORDER_DETAILS = (id: string) => `${ORDER_CHECKOUT}/${id}`;
 }
 
 export { default as Currencies } from "./currencies";
